96ccdb1db70f115301c45b003d766b393b5160e8
Qurial <79192846+Qurial@users.noreply.github.com>
2021-06-18 23:10:31 +0300

    Add files via upload

---
+++ CLab6(17)/CLab6(17)/CLab6(17).c
@@ -0,0 +1,95 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int item;
+	struct node* left;
+	struct node* right;
+};
+
+void PrintTree(struct node* root)
+{
+	if (root == NULL) return;
+	PrintTree(root->left);
+	printf("%d ", root->item);
+	PrintTree(root->right);
+}
+
+struct node* createNode(value)
+{
+	struct node* newNode = malloc(sizeof(struct node));
+	newNode->item = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return newNode;
+}
+
+struct node* insertLeft(struct node* root, int value)
+{
+	root->left = createNode(value);
+	return root->left;
+}
+
+struct node* insertRight(struct node* root, int value)
+{
+	root->right = createNode(value);
+	return root->right;
+}
+
+/*struct node* addBalanced(struct node* root, int value)
+{
+	if (root == NULL)
+	{
+		root = createNode(value);
+		return root;
+	}
+	
+	else if (root->item != NULL && value > root->item)
+	{
+		addBalanced(root->right, value);
+	}
+	else if (root->item != NULL && value < root->item)
+	{
+		addBalanced(root->left, value);
+	}
+}*/
+struct node* insert(struct node* node, int value) {
+	// Return a new node if the tree is empty
+	if (node == NULL) return createNode(value);
+
+	// Traverse to the right place and insert the node
+	if (value < node->item)
+		node->left = insert(node->left, value);
+	else if (value > node->item)
+		node->right = insert(node->right, value);
+
+	return node;
+}
+
+void balanceTree(struct node* root, struct node* balancedRoot)
+{
+	//balancedRoot = createNode(root->item);
+	if (root == NULL) return;
+	balanceTree(root->left, balancedRoot);
+	insert(balancedRoot, root->item);
+	balanceTree(root->right, balancedRoot);
+}
+
+int main() {
+	struct node* root = createNode(1);
+	insertLeft(root, 2);
+	insertRight(root, 3);
+	insertLeft(root->left, 4);
+	insertRight(root->left, 5);
+	insertLeft(root->right, 6);
+
+	printf("\Not balanced tree:\n");
+	PrintTree(root);
+
+	struct node* BalancedRoot = createNode(root->item);
+	balanceTree(root, BalancedRoot);
+
+	printf("\nBalanced tree:\n");
+	PrintTree(BalancedRoot);
+}
---
+++ CLab7(28)/CLab7(28)/CLab7(28).c
@@ -0,0 +1,118 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+#pragma warning (disable : 4996)
+
+int PrintPlaneInfo(int flightNum)
+{
+	char numStr[10];
+	const unsigned MAX_LENGTH = 256;
+	char buffer[256];
+	itoa(flightNum, numStr, 10);
+	char link[30] = "planes/plane";
+	strcat(link, numStr);
+	strcat(link, ".txt");
+	FILE* fp = fopen(link, "r");
+	if (!fp) { return  1; }
+	else
+	{
+		while (fgets(buffer, MAX_LENGTH, fp)) { printf("%s", buffer); }
+		fclose(fp);
+	}
+	return 0;
+}
+void PrintSchedule()
+{
+	int flNum = 0, isError = 0;
+	while (isError == 0)
+	{
+		flNum++;
+		isError = PrintPlaneInfo(flNum);
+		printf("\n");
+	}
+}
+void PrintByCity(char city[50])
+{
+	int flNum = 0, isError = 0;
+	while (isError == 0)
+	{
+		flNum++;
+		char numStr[10];
+		const unsigned MAX_LENGTH = 256;
+		char buffer[256];
+		itoa(flNum, numStr, 10);
+		char link[30] = "planes/plane";
+		strcat(link, numStr);
+		strcat(link, ".txt");
+		FILE* fp = fopen(link, "r");
+		char ccity[256] = "";//"Arrival point ";
+		strcat(ccity, city);
+		strcat(ccity, "\n");
+		if (!fp) { isError = 1; }
+		else
+		{
+			while (fgets(buffer, MAX_LENGTH, fp))
+			{
+				if (printf(buffer) == printf(ccity))
+				{
+					printf("\x1b[1F");
+					printf("\x1b[2K");
+					printf("\x1b[1F");
+					printf("\x1b[2K");
+					PrintPlaneInfo(flNum); printf("\n");
+					free(buffer); free(ccity);
+					break;
+				}
+				else
+				{
+					printf("\x1b[1F");
+					printf("\x1b[2K");
+					printf("\x1b[1F");
+					printf("\x1b[2K");
+				}
+			}
+		}
+	}
+}
+void BuyTickets(int flightNum, int ticketsNum)
+{
+	char numStr[10];
+	const unsigned MAX_LENGTH = 256;
+	char buffer[256];
+	itoa(flightNum, numStr, 10);
+	char link[30] = "planes/plane";
+	strcat(link, numStr);
+	strcat(link, ".txt");
+	FILE* fp = fopen(link, "r");
+	FILE* fTemp;
+	char path[100];
+	int count = 0;
+
+	char newline[256];
+
+
+	while ((fgets(buffer, 256, fp)) != NULL)
+	{
+		count++;
+		if (count == 7) 
+		{
+			itoa((atoi(buffer) - ticketsNum), newline, 10);
+			fputs(newline, fTemp);
+		}
+		else
+		{
+			fputs(buffer, fTemp);
+		}
+	}
+
+	fclose(fp);
+	fclose(fTemp);
+}
+
+int main()
+{
+	PrintPlaneInfo(1);
+	PrintSchedule();
+	PrintByCity("Minsk");
+	BuyTickets(1, 2);
+}
